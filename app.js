ad63ad4f0638f9d3ad8f3b0aa19876a90d899bee
KHUSHI-AGRAWAL-1 <khushi.agrawal_cs21@gla.ac.in>
2024-07-20 16:24:29 +0530

    added

--- app.js
+++ app.js
@@ -10,52 +10,38 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
-// Session--------------------------------- //instal  express-session and se before passport
+
 const session = require("express-session");
 app.use(
   session({
-    secret: "krishna", // Replace with your own secret key
+    secret: "krishna",
     resave: false,
     saveUninitialized: false,
   })
 );
 
-// _______________________________
-
-//first install than import for passport-------
 const User = require("./model/user");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-// ----------------------
 
-//PASSPORT----------------------------
-//midllewares
-//1 Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-// 2copy from PLM...
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-// ----------------------------------------------------------
-
-//self created middleware
 app.use((req, res, next) => {
-  // console.log(req.user);
-  res.locals.currentUser = req.user; //to store curent user who log in info in local storage
+  res.locals.currentUser = req.user;
   next();
 });
 
-//set some property
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.engine("ejs", ejsMate); //instal andd req first and then set engin for ejs as ejs mate
+app.engine("ejs", ejsMate); 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method")); //over ride post request of update to patch request
+app.use(methodOverride("_method")); 
 
-//if create rout the use route in app.js
 const productrouts = require("./route/productroute");
 app.use(productrouts);
 
--- route/auth.js
+++ route/auth.js
@@ -1,33 +1,22 @@
 const express=require("express");
-const passport = require('passport'); // for using authenticate middleware and .register 
+const passport = require('passport'); 
 const User = require("../model/user");
 const router=express.Router();
 
-//1 .signup-------------->
 router.get('/register',(req,res)=>{
     res.render('auth/signup.ejs');
 });
 
-  //add the user data to userDB
 router.post('/register',async(req,res)=>{
     const {email,password,username}=req.body;
     const user=new User({email,username});
-    const newuser=await User.register(user,password);//add slat with passward and save it in hash form user DB
-    // res.send(newuser);
+    const newuser=await User.register(user,password);
     res.redirect('/login');
 });
-
-//signup end---------------------->
-
-
-
-
-//login part----------------->
 router.get('/login',(req,res)=>{
     res.render('auth/login.ejs');
 });
 
-  //add the user data to userDB
 router.post('/login',
 passport.authenticate('local', { failureRedirect: '/login' })
 ,(req,res)=>{
@@ -35,19 +24,10 @@ passport.authenticate('local', { failureRedirect: '/login' })
     res.redirect('/products');
 });
 
-
-
-
-
-
-//logout part ----------------->
 router.get('/logout',(req,res)=>{
    ()=>{
     req.logout(); 
-  }//always call in callback fun
+  }
    res.redirect('/login');
 });
-
-
-
 module.exports=router;
